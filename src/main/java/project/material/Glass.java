d0fbf553a06655a1732eab1bf5ae9f52512cbddd
Natallia Malazhavaya <natasha281006@gmail.com>
2020-10-17 13:56:12 +0300

    Delete classes Metal, Plastic, Glass

--- src/main/java/project/material/Glass.java
+++
@@ -1,7 +0,0 @@
-package main.java.project.material;
-
-public class Glass extends Material {
-    public Glass(double thermalConductivity, String color, double density) {
-        super(thermalConductivity, color, density);
-    }
-}
